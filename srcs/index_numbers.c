478833283a45aff790edb10b83b77b92f14e91e5
Jamal <jamalchdid2018@gmail.com>
2025-04-03 22:58:32 +0000

    Update index_numbers.c

--- srcs/index_numbers.c
+++ srcs/index_numbers.c
@@ -11,7 +11,53 @@
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+/*static void compute_ranks(int *arr, int *ranks, int size)
+{
+    int i = 0, j;
 
+    while (i < size)
+    {
+        ranks[i] = 0;
+        i++;
+    }
+
+    i = 0;
+    while (i < size)
+    {
+        j = 0;
+        while (j < size)
+        {
+            if (arr[j] < arr[i])
+                ranks[i]++;
+            j++;
+        }
+        i++;
+    }
+}
+
+void fill_index(int *arr, int size)
+{
+    int *ranks;
+    int i = 0;
+
+    if (size <= 0)
+        return;
+
+    ranks = malloc(sizeof(int) * size);
+    if (!ranks)
+        quit_error();
+
+    compute_ranks(arr, ranks, size);
+
+    i = 0;
+    while (i < size)
+    {
+        arr[i] = ranks[i];
+        i++;
+    }
+
+    free(ranks);
+}*/
 void	fill_index(int *arr, int size)
 {
 	int *ranks;
